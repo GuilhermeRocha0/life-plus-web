75f45d49bd594555848684412b00db4219800226
GuilhermeRocha0 <gui.rocha009@gmail.com>
2025-11-02 09:21:56 -0300

    feat: reset password, loading and message modal
    feature:
    - reset password page
    - reset password route
    - message modal component
    - loading modal component

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import Profile from '../pages/Profile.tsx'
 import Exams from '../pages/Exams.tsx'
 import Medicines from '../pages/Medicines.tsx'
-import Home from '../pages/Home.tsx';
-import Login from '../pages/Login.tsx';
-import Cadastro from '../pages/Register.tsx';
+import Home from '../pages/Home.tsx'
+import Login from '../pages/Login.tsx'
+import Cadastro from '../pages/Register.tsx'
+import ResetPassword from '../pages/ResetPassword.tsx'
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/redefinir-senha" element={<ResetPassword />} />
       <Route path="/cadastro" element={<Cadastro />} />
       <Route path="/perfil" element={<Profile />} />
       <Route path="/exames" element={<Exams />} />
       <Route path="/remedios" element={<Medicines />} />
-      {/* Rota padrão */}
       <Route path="*" element={<Profile />} />
     </Routes>
-  );
-};
+  )
+}
 
-export default AppRoutes;
+export default AppRoutes
